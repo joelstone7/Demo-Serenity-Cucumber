9588443c6dc28c78c75f79b2eca62ccc9da64e9e
joelstone7 <cbbe.joelademar.castro.ga@unifranz.edu.bo>
2026-05-27 22:06:02 -0400

    Update ValidationPage.java

--- src/test/java/com/swag/pe/pages/validations/ValidationPage.java
+++ src/test/java/com/swag/pe/pages/validations/ValidationPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.support.FindBy;
 
 public class ValidationPage extends PageObject {
     @FindBy(xpath = "//div[@class='product_label']")
+     @FindBy(xpath = "//span[@class='title']")
     protected WebElementFacade lbl_product;
-    @FindBy(css="h3[data-test='error']")
     protected WebElementFacade lbl_errorMessage;
 
 }
